eab4d41b45dac2e09b593c18193e25430761294f
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-10-13 00:09:12 +0500

    add v.3.1

--- Project/v.3.1/script/index.js
+++ Project/v.3.1/script/index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    // Initialization Materalize
     let el = document.querySelector(".tabs")
-    let instance = M.Tabs.init(el);
+    let instance = M.Tabs.init(el)
+
 });
