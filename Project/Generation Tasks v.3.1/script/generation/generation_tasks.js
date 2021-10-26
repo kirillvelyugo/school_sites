103743ffa5cedfd5be903195571987e714594acd
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-10-26 09:35:43 +0500

    pres

--- Project/Generation Tasks v.3.1/script/generation/generation_tasks.js
+++ Project/Generation Tasks v.3.1/script/generation/generation_tasks.js
@@ -1,9 +1,9 @@
 function generate() {
     // Generation 1 type
     if (data.type_1.choose_type) {
-        document.getElementById('type_1_print-block').classList.remove('_hide');
+        document.getElementById('type_1_print-block').classList.remove('_hide')
 
-        let task_block = "", answer_str = ""
+        let task_block = '', answer_str = ''
         for (let c = 1; c <= data.type_1.count_task; c++) {
             let type_1_data = type_1_get_data(c)
             task_block += type_1_get_text(type_1_data)
@@ -21,9 +21,9 @@ function generate() {
 
     // Generation 2 type
     if (data.type_2.choose_type) {
-        document.getElementById('type_2_print-block').classList.remove('_hide');
+        document.getElementById('type_2_print-block').classList.remove('_hide')
 
-        let task_block = "", answer_str = ""
+        let task_block = '', answer_str = ''
         for (let c = 1; c <= data.type_2.count_task; c++) {
             let type_2_data = type_2_get_data(c)
             task_block += type_2_get_text(type_2_data)
@@ -42,9 +42,9 @@ function generate() {
 
     // Generation 3 type
     if (data.type_3.choose_type) {
-        document.getElementById('type_3_print-block').classList.remove('_hide');
+        document.getElementById('type_3_print-block').classList.remove('_hide')
 
-        let task_block = "", answer_str = ""
+        let task_block = '', answer_str = ''
         for (let c = 1; c <= data.type_3.count_task; c++) {
             let type_3_data = type_3_get_data(c)
             task_block += type_3_get_text(type_3_data)
